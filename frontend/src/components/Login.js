9ef1c3efc3796890b18ea8904d60c8019efd0109
bhakti ghevariya <bhg.globalia@gmail.com>
2025-09-04 16:11:29 +0530

    Setup React frontend app with basic pages and routing

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,3 @@
+
+git add frontend/src/components/Login.js
+git commit -m "Add Login component for user authentication"
---
+++ frontend/src/components/Register.js
@@ -0,0 +1,2 @@
+git add frontend/src/components/Register.js
+git commit -m "Add Register component for user signup"
